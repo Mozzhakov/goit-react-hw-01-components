09c04c97e011970088f15186351e20316d5ce29c
Mozzhakov <imozzhakov@icloud.com>
2023-01-30 12:29:04 +0200

    update statistics title

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 export const Statistics = ({ title, stats }) => {
   return (
     <section className={css.statistics}>
-      <h2 className={css.title}>{title}</h2>
+      {title ? <h2 className={css.title}>{title}</h2> : ''}
       <ul className={css.statList}>
         {stats.map(({ id, label, percentage }) => (
           <StatsItem key={id} label={label} percentage={percentage} />
